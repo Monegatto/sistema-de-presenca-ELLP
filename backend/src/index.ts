407c6dbd7948f4cbddfbeb9854c4d388a2ecbf5e
iTrue <109562378+carlosyamanaka@users.noreply.github.com>
2025-05-14 13:08:31 -0300

    initialized backend files

---
+++ backend/src/index.ts
@@ -0,0 +1,36 @@
+import express from 'express'
+import { PrismaClient } from '@prisma/client'
+import { withAccelerate } from '@prisma/extension-accelerate'
+
+const app = express()
+const prisma = new PrismaClient().$extends(withAccelerate())
+
+async function main() {
+  const user = await prisma.user.create({
+    data: {
+      name: 'Alice',
+      email: 'alice1@prisma.io',
+    },
+  })
+  console.log(user)
+}
+
+main()
+  .then(async () => {
+    await prisma.$disconnect()
+  })
+  .catch(async (e) => {
+    console.error(e)
+    await prisma.$disconnect()
+    process.exit(1)
+  })
+
+app.use(express.json())
+
+app.get('/ping', (_, res) => {
+  res.send('pong')
+})
+
+app.listen(3000, () => {
+  console.log('Servidor rodando na porta 3000')
+})
